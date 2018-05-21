ef00db0e2f573ecf2163c5b206220a1688aae980
MufidJamaluddin <mufid.jamaluddin@gmail.com>
2018-05-21 23:07:49 +0700

    Change Controller

--- controllers/home.py
+++ controllers/home.py
@@ -1,9 +1,25 @@
 from odoo import http
+from odoo.http import request
 
 class Home(http.Controller) :
 
-    @http.route('/travel/home', auth='public', website=True)
-    def index(self, **kw) :
-        return http.request.render('travel.home', {
-            'cities' : ["Bandung", "Jakarta", "Surabaya", "Makassar", "Jayapura"],
-        })
+    @http.route('/travel/home', type='http', auth='public', methods=['GET'], website=True)
+    def get_index(self, **kw) :
+		
+		uid = request.session.uid
+		
+		if uid is not None:
+		
+			partners = request.env['res.partner'].search(['user_id', '=', uid])
+			
+			partner_id = request.env['res.users'].browse(uid).partner_id
+
+			travels = request.env['travel.order'].search(['partner_id','=', partner_id])
+			
+			return request.render('travel.home', {
+				'partners' : partners,
+				'travels' : travels,
+			})
+		
+		else:
+			return request.render('travel.login')
