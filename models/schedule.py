bd24b7227a1fb4a84537a06c432070cc985826dc
Fadhil226 <muhammad.fadhil.tif16@polban.ac.id>
2018-06-04 10:43:39 +0700

    Schedule pool repair

--- models/schedule.py
+++ models/schedule.py
@@ -3,27 +3,28 @@ from odoo import models, fields, api
 
 class TravelSchedule(models.Model):
 	_name = 'travel.schedule'
-	
+
 	departure = fields.Many2one('travel.pool.city', required=True)
 	destination = fields.Many2one('travel.pool.city', required=True)
 	departure_date = fields.Date('Departure Date',required=True)
-	departure_time = fields.Float('Departure Time',required=True)
+	# departure_time = fields.Float('Departure Time',required=True)
 	vehicle = fields.Many2one('fleet.vehicle', required=True)
 	order_list = fields.One2many('travel.order', 'schedule_id')
 	pool_list = fields.One2many('travel.pool.line', 'schedule')
-	
+
 class PoolLine(models.Model):
 	_name = 'travel.pool.line'
 	schedule = fields.Many2one('travel.schedule', string="Schedule")
 	pool_location = fields.Many2one('travel.pool.place',ondelete='cascade')
 	name = fields.Char(compute="_compute_pool_name", store=False)
-	
+	departure_perpool = fields.Float('Departure Time')
+
 	@api.multi
 	def _compute_pool_name(self):
 		for record in self:
 			record.name = record.pool_location.city_ids.city + '/' + record.pool_location.address
-	
+
 #class Vehicle(models.Model):
 #	_inherit = 'fleet.vehicle'
-	
+
 #	schedule_id = fields.Many2one('travel.schedule')
